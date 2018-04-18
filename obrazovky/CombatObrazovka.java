ab7ca977f536a5503a0011722190483381fe70bb
slovakiaman <Marek@DESKTOP-0THLNSI.mshome.net>
2018-04-19 01:25:17 +0200

    Konkretizované niektoré triedy, CombatObrazovka WIP a veľa veľa otázok

--- obrazovky/CombatObrazovka.java
+++ obrazovky/CombatObrazovka.java
@@ -5,18 +5,84 @@
  */
 package sk.uniza.fri.liteworld.obrazovky;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import sk.uniza.fri.liteworld.Hrac;
+import sk.uniza.fri.liteworld.npc.Nepriatel;
+
 /**
  *
  * @author Marek
  */
 public class CombatObrazovka extends AbstractObrazovka {
 
-    public CombatObrazovka(/*INepriatel nepriatel*/) {
+    private static Hrac hrac;
+    private static Nepriatel nepriatel;
+    private static boolean ideHrac;
+    
+    public CombatObrazovka(Hra hra, final Nepriatel nepriatel) {
+        hrac = hra.getHrac();
+        float povX = hrac.getX();
+        float povY = hrac.getY();
+        hrac.setPosition(100, 100);
+        
+        
+        ImageButton buttonUtoc = UIPrvky.vytvorButton(new Texture("buttons/btn_play.png"));
+        addActor(buttonUtoc);
+        buttonUtoc.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                hrac.utoc(nepriatel);
+                ideHrac = false;
+                return false;
+            }
+        });/*
+        ImageButton buttonNieco = UIPrvky.vytvorButton(new Texture("buttons/btn_play.png"));
+        addActor(buttonUtoc);
+        buttonUtoc.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                hrac.utoc(nepriatel);
+                return false;
+            }
+        });*/
+        
+        //true = hrac , false = nepriatel
+        ideHrac = hrac.getRychlost() >= nepriatel.getRychlost();
+        int rychlostHrac = hrac.getRychlost();
+        int rychlostNepriatel = nepriatel.getRychlost();
+        while (true) {
+            //this.buildStage();
+            hrac.draw(hra.getBatch());
+            nepriatel.vykresliSa(hra.getBatch());
+            buttonUtoc.setVisible(ideHrac);
+            if (ideHrac) {
+                //vyriešiť aby hráč najskôr musel spraviť interakciu, až potom sa to preplo -- DONE
+                if (nepriatel.getHp() < 1) {
+                    System.out.println("Nepriatel umrel");
+                    break;
+                }
+            } else {
+                nepriatel.interakcia(hrac);
+                if (hrac.getHp() < 1) {
+                    System.out.println("Umrel si");
+                    break;
+                }
+                ideHrac = true;
+            }
+            //vypocitavanie kto ma ist na tah  --- PRIDAŤ
+            //to treba lepšie premyslieť
+            
+            
+        }
         
     }
     
     @Override
     public void buildStage() {
+        
     }
     
 }
